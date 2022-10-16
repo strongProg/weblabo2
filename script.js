e1c00827b4e7f2de37e5c85525b2f6782d8d93af
strongProg <111303284+strongProg@users.noreply.github.com>
2022-10-16 02:53:49 -0400

    weblabo2 commit

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
 import Deck from "./deck.js"
 
+
+const listeCarte  = document.querySelector(".computer-card-slot")
+const playerDeck = document.querySelector('.player-deck')
+const texte = document.querySelector('.text') 
+
 const deck = new Deck()
-console.log(deck.cards)
 
-const list  = document.querySelector(player-card-slot)
-const Text = document.querySelector('.text') 
+listeCarte.appendChild(deck.cards[0].getHTML())
 
-let joueur1, joueur2
 
-demarrer_jeux()
-function demarrer_jeu() {
-    const deck = new Deck()
-    desk.brassage()
 
-    const deckCouper = Math.ceil(deck.nbreCartes/2)
-    joueur1 = new Deck(deck.cartes.slice(0, deckCouper))
-    joueur2 = new Deck(deck.cartes.slice(deckCouper, nbreCartes))
-    console.log(joueur1)
-    console.log(joueur2)
 
-/*netoyer_ecran()*/
-}
+
 
 
