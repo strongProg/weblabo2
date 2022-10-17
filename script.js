4ac453d6a964deb55eef7ab10c47f023d419ac65
strongProg <111303284+strongProg@users.noreply.github.com>
2022-10-17 00:56:16 -0400

    commit laboweb

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 import Deck from "./deck.js"
 
 
-const listeCarte  = document.querySelector(".computer-card-slot")
-const playerDeck = document.querySelector('.player-deck')
-const texte = document.querySelector('.text') 
+const listeCarte  = document.querySelector(".player-card-slot")
+const playerDeck = document.querySelector(".player-deck")
+const texte = document.querySelector(".text") 
 
 const deck = new Deck()
-
-listeCarte.appendChild(deck.cards[0].getHTML())
-
-
-
-
-
-
+deck.brassage
+listeCarte.appendChild(deck.cards[0].getHTML())  
 
